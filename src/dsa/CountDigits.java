53e7c0ca933dddd0db225d42ccab918724b3a326
narmada-git <pnarmada697@gmail.com>
2021-11-03 10:57:48 +0530

    created findCountDigit function

--- src/dsa/CountDigits.java
+++ src/dsa/CountDigits.java
@@ -3,8 +3,19 @@ package dsa;
 public class CountDigits {
 
     public static void main(String[] args) {
-        int n = 432;
-        
+        int n = 43872;
+        CountDigits countDigits = new CountDigits();
+        int ans = countDigits.findCountDigit(n);
+        System.out.println("ans = " + ans);
+    }
+
+    int findCountDigit(long n) {
+        int count = 0;
+        while (n != 0) {
+            n = n / 10;
+            ++count;
+        }
+        return count;
     }
 
 }
